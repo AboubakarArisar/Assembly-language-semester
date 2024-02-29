fdd1c18c23a6f3f99e91b22978912b9ba0100feb
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-02-29 11:23:50 +0500

    steric pattern in assembly

---
+++ Lab4/task7.asm
@@ -0,0 +1,37 @@
+.model small
+.stack 100h
+.data
+
+iter dw 1
+val db '*'
+
+.code
+
+main proc
+    mov ax, @data
+    mov ds, ax
+    mov cx, 5
+   
+loop1:
+    PUSH cx
+    mov cx, iter
+
+loop2:
+    mov ah, 2
+    mov dl, val
+    int 21h  
+    loop loop2
+
+    mov ah, 2
+    mov dl, 10
+    int 21h 
+    mov dl, 13
+    int 21h
+    inc iter
+    POP cx
+    loop loop1
+
+    mov ah, 4ch
+    int 21h
+main endp
+end main
