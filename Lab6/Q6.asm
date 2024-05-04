bed20be269a1cc11355fc99af994c18d73588c5f
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-05-04 15:50:43 +0500

    Create Q6.asm

---
+++ Lab6/Q6.asm
@@ -0,0 +1,130 @@
+.model small
+.stack 100h
+
+.data
+      strr        db 'Do you want to repeat operation : $'
+      str         db 'Enter number 1: $'
+      str2        db 'Enter number 2: $'
+      str3        db 'Choose operator (+ or -): $'
+      addition    db 'Addition is: $'
+      subtraction db 'Subtraction is: $'
+      n1          db 0
+      n2          db 0
+      res         db 0
+      newline     db 10, 13, '$'
+
+.code
+main proc
+            mov ax, @data
+            mov ds, ax
+      lbl:  
+            mov ah,9h
+            lea dx, newline
+            int 21h
+          
+            mov ah, 9h
+            lea dx, str
+            int 21h
+
+            mov ah,1
+            int 21h
+            sub al,'0'
+            mov n1,al
+
+            mov ah,9
+            lea dx,newline
+            int 21h
+
+            mov ah, 9h
+            lea dx, str2
+            int 21h
+
+            mov ah,1
+            int 21h
+            sub al,'0'
+            mov n2,al
+
+            mov ah,9
+            lea dx,newline
+            int 21h
+
+            mov ah, 9h
+            lea dx, str3
+            int 21h
+
+            mov ah,1
+            int 21h
+            mov bl,al
+            cmp bl,'+'
+            je  addt
+            cmp bl,'-'
+            je  subt
+
+
+      again:
+    
+            mov ah,9h
+            lea dx, newline
+            int 21h
+
+            mov ah, 9h
+            lea dx, strr
+            int 21h
+
+            mov ah, 1
+            int 21h
+            cmp al, 'y'
+            je  lbl
+            cmp al, 'Y'
+            je  lbl
+          
+         
+            jmp exit
+      addt: 
+            mov al, n1
+            add al, n2
+            mov res, al
+         
+            mov ah,9h
+            lea dx, newline
+            int 21h
+         
+            mov ah, 9h
+            lea dx, addition
+            int 21h
+
+            mov ah, 2
+            mov dl, res
+            add dl, '0'
+            int 21h
+            jmp again
+
+
+
+      subt: 
+            mov al, n1
+            sub al, n2
+            mov res, al
+
+            mov ah,9h
+            lea dx, newline
+            int 21h
+         
+            mov ah, 9h
+            lea dx, subtraction
+            int 21h
+         
+            mov ah, 2
+            mov dl, res
+            add dl, '0'
+            int 21h
+            jmp again
+
+ 
+
+      exit: 
+            mov ah, 4ch
+            int 21h
+
+main endp
+end main
