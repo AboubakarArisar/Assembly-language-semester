a29e314c51ff634eb2f870694af052ba7f424b3c
Aboubakar <116747262+AboubakarArisar@users.noreply.github.com>
2024-05-04 15:46:25 +0500

    loops and labels in assembly

---
+++ Lab6/Q3.asm
@@ -0,0 +1,53 @@
+.model small
+.stack 100h
+
+.data
+    str     db 'Enter the sequence of characters : $'
+    str2    db 'Number of Small Alphabets: $'
+    ct      db 0
+    newline db 10, 13, '$'
+
+.code
+main proc
+          mov ax, @data
+          mov ds, ax
+
+          mov ah, 9h
+          lea dx, str
+          int 21h
+
+    lbl:  
+          mov ah, 1
+          int 21h
+          mov bl, al
+
+          cmp bl, 13
+          je  lbl2
+              
+          cmp bl,'a'
+          jl  large
+          
+
+          inc ct
+
+    large:
+          jmp lbl
+    lbl2: 
+
+          mov ah, 9h
+          lea dx, str2
+          int 21h
+
+          mov ah, 2
+          mov dl, ct
+          add dl,48
+          int 21h
+          jmp exit
+
+
+    exit: 
+          mov ah, 4ch
+          int 21h
+
+main endp
+end main
